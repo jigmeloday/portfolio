ffdd39cdce9f77d785b5a4ad36e869ea504b5b4f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-04 21:55:47 +0600

    theme set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-
 export default function Home() {
   return (
     <main>
-      <div className='text-red'>
-          jjj
+      <div className='min-h-screen bg-primary-main text-main'>
+          <h1 className='font-9'>hello</h1>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,34 @@ module.exports = {
   ],
   theme: {
     extend: {
-      primary: {
-        main: '#fae20a',
-        light: '#fcee6c',
-        dark: '#e1cb09'
+      fontWeight: {
+        1: '100',
+        2: '200',
+        3: '300',
+        4: '400',
+        5: '500',
+        6: '600',
+        7: '700',
+        8: '800',
+        9: '900'
       },
-      background: {
-        main: '#1a1a1a',
-        light: '#333333',
-        dark: '#000000'
-      }
+      colors: {
+        main: '#adadad',
+        light: '#dedede',
+        dark: '#797979'
+      },
+      backgroundColor: {
+        primary: {
+          main: '#1a1a1a',
+          light: '#333333',
+          dark: '#000000'
+        },
+        secondary: {
+          main: '#fae20a',
+          light: '#fcee6c',
+          dark: '#e1cb09',
+        },
+      },
     },
   },
   plugins: [],
