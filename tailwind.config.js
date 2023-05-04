10ce9cb61c8541ba2c3e8dafe115f33254327dfc
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-04 19:43:39 +0600

    theme set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      primary: {
+        main: '',
+        light: '',
+        dark: ''
       },
+      background: {
+        main: '',
+        light: '',
+        dark: ''
+      }
     },
   },
   plugins: [],
