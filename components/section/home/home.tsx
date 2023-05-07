14a445f8ae617cb95ca97c9dcc4461aab8f5c873
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-07 18:56:52 +0600

    color theme setting

--- components/section/home/home.tsx
+++ components/section/home/home.tsx
@@ -6,13 +6,14 @@ function Landing(){
             <div className='flex items-center justify-center bg-primary-dark shadow-2xl  rounded-full w-[200px] h-[200px] ' >
                 profile
             </div>
+
             <div className='pt-[38px] text-center'>
                 <h1 className='text-[40px] font-8'> Jigme Lodey </h1>
                 <span className='pt-[8px] text-[24px]'> I am Software <span className='text-primary-main'>Developer</span> </span>
                 <div className='py-[30px] flex gap-[18px] justify-center'>
                     {
                         SOCIAL_ICONS.map(({ id, name }) => (
-                            <div key={id} className='text-[34px] cursor-pointer text-primary-main hover:scale-150 translation duration-300 ' >
+                            <div key={id} className='text-[34px] cursor-pointer shadow-2xl text-primary-main hover:scale-150 translation duration-300 ' >
                                 {name}
                             </div>
                         ))
