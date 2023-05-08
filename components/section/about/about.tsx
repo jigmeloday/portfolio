e799a632720fa7431bef637637f5f9ffaddc9ec9
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-08 22:15:21 +0600

    about animation

--- components/section/about/about.tsx
+++ components/section/about/about.tsx
@@ -1,8 +1,20 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
 function About() {
     return(
-        <div className='min-h-screen flex justify-center'>
-            hello
-        </div>
+        <AnimatePresence>
+            <div className='min-h-screen flex justify-center '>
+                <motion.div
+                    initial={{ opacity: 0, y: -100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true, amount: 0.8 }}
+                    transition={{ type: 'spring', bounce: 0.1 }}
+                >
+                    hello
+                </motion.div>
+
+            </div>
+        </AnimatePresence>
     );
 }
 
