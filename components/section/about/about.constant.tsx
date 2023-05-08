0a75efb22b5cd348768a38e67e10e2a7a789dc92
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-08 23:28:50 +0600

    about boxing

--- components/section/about/about.constant.tsx
+++ components/section/about/about.constant.tsx
@@ -1,23 +1,27 @@
 import { AboutModel } from '@/components/section/about/about.model';
+import { MdOutlineDeveloperMode } from 'react-icons/md';
+import { HiCode } from 'react-icons/hi';
+import { FaDesktop } from 'react-icons/fa';
+
 
 export const DUMMY: string = 'Hey there! My name is Jigme Lodey and I am a passionate software developer from Thimphu, Bhutan. As a full stack developer, I leverage my skills in both front-end and back-end technologies to create robust and dynamic software solutions. I first fell in love with programming while studying IT in high school, and pursued my passion by majoring in Computer Science. Since then, I have been dedicated to exploring the endless possibilities of the programming world, constantly learning and evolving my skills. Whether it\'s building websites, web-apps, or mobile apps, I always strive to create innovative and user-friendly software that exceeds expectations. If you have a project that needs a tech-savvy touch, let\'s work together to bring it to life!';
 export const ABOUT_BOX: AboutModel[] = [
     {
         id: '1',
-        icon: <></>,
-        title: 'test',
+        icon: <MdOutlineDeveloperMode/>,
+        title: 'Mobile Application',
         description: 'hello'
     },
     {
         id: '2',
-        icon: <></>,
-        title: 'test',
+        icon: <HiCode/>,
+        title: 'Web Application',
         description: 'hello'
     },
     {
         id: '2',
-        icon: <></>,
-        title: 'test',
+        icon: <FaDesktop/>,
+        title: 'System Application',
         description: 'hello'
     }
 ];
--- components/section/about/about.tsx
+++ components/section/about/about.tsx
@@ -30,12 +30,20 @@ function About() {
                         transition={{ type: 'spring', bounce: 0.1, delay: 0.7 }}
                         className='flex flex-row py-8 justify-center gap-[38px]'>
                         {
-                            ABOUT_BOX.map(({ id,  title, description }) => (
+                            ABOUT_BOX.map(({ id, icon,  title, description }) => (
                                 <div
                                     key={id}
                                     className='w-[250px] h-[200px] overflow-hidden transition duration-150 group relative rounded-[16px]'
                                 >
-                                    <div className='min-h-full bg-primary-main shadow-2xl relative overflow-hidden'>{title}</div>
+                                    <div className='flex flex-col min-h-full bg-primary-main shadow-2xl relative overflow-hidden justify-center items-center'>
+                                        <div className='text-primary-main'>
+                                            {icon}
+
+                                        </div>
+                                        <span>
+                                            {title}
+                                        </span>
+                                    </div>
                                     <div className='min-h-full w-full bg-hover-main absolute flex items-center justify-center top-60 group-hover:top-0 opacity-0 group-hover:opacity-100 transition-all duration-500'>
                                         {description}
                                     </div>
