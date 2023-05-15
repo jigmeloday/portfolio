5409d9e1803083589b1be158c565adf499ceca86
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-15 21:26:20 +0600

    stepper for exp

--- components/section/exp/stepper-component/stepper-component.tsx
+++ components/section/exp/stepper-component/stepper-component.tsx
@@ -1,48 +1,57 @@
 import { useState } from 'react';
 import { EXP_DUMMY } from '@/components/section/exp/constant/exp.constant';
 import Chip from '@/components/chip/chip';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function StepperComponent() {
     const [ open, setOpen ] = useState( '1' );
     return (
-        <ul className='relative m-0 w-full list-none overflow-hidden p-0 transition-[height] duration-200 ease-in-out'>
+        <ul className='relative m-0 w-full list-none overflow-hidden p-0 transition duration-200 ease-in-out'>
             {
                 EXP_DUMMY.map( ( { id, title, joined, ended, stack }, index ) =>
                     (
                         <li key={ id } onClick={ () => setOpen( id ) }
-                            className={ `relative h-fit ${ (EXP_DUMMY.length !== index + 1 || open === id) && 'after:absolute after:left-[30px] after:top-[56px] after:mt-px after:h-[calc(100%-2.45rem)] after:w-px after:bg-secondary-main  dark:after:bg-secondary-main' }` }>
-                            <div
-                                className='flex items-center p-6 leading-[1.3rem] no-underline after:bg-secondary-main focus:outline-none dark:after:bg-secondary-main '>
+                            className={ `relative h-fit ${ EXP_DUMMY.length !== index + 1  && 'after:absolute after:left-[30px] after:top-[56px] after:mt-px after:h-[calc(100%-2.45rem)] after:w-px after:bg-secondary-main  ' } transition duration-200 ease-in-out` }>
+                            <div className='flex items-center p-6 leading-[1.3rem] no-underline after:bg-secondary-main focus:outline-none transition duration-200 ease-in-out'>
+                                <span className='mr-3 cursor-pointer flex h-[14px] w-[14px] items-center justify-center rounded-full bg-secondary-light transition duration-200 ease-in-out '/>
                                 <span
-                                    className='mr-3 cursor-pointer flex h-[14px] w-[14px] items-center justify-center rounded-full bg-secondary-light '/>
-                                <span
-                                    className={ `cursor-pointer font-2 ${ open === id ? 'text-primary-main' : 'text-text-main ' }` }>
+                                    className={ `cursor-pointer font-2 ${ open === id ? 'text-primary-main' : 'text-text-main ' } transition duration-200 ease-in-out` }>
                                     { title }
                                 </span>
                             </div>
-                            <div className='ml-6 px-6'>
+                            <AnimatePresence>
                                 {
                                     open === id &&
-                                    <div className='w-[80%]'>
-                                        <span className='font-6 text-[22px] text-text-light'>Frontend Developer</span>
-                                        <div className='pt-[12px] pb-[6px] font-3 text-[14px] text-text-dark'>
-                                            <span>{ `${ joined } - ${ ended || 'Ongoing' }` }</span>
-                                        </div>
-                                        <span className='text-text-main text-[16px] font-3'>
+                                    <motion.div
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1}}
+                                        transition={{ duration: 0.8 }}
+                                        className='ml-6 px-6 transition-[height] duration-200 ease-in-out'>
+                                        <div className='w-[80%] transition duration-200 ease-in-out'>
+                                            <span className='font-6 text-[22px] text-text-light'>Frontend Developer</span>
+                                            <div className='pt-[12px] pb-[6px] font-3 text-[14px] text-text-dark'>
+                                                <span>{ `${ joined } - ${ ended || 'Ongoing' }` }</span>
+                                            </div>
+                                            <span className='text-text-main text-[16px] font-3'>
                                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                        </span>
-                                        <div className='flex gap-[12px] pt-4'>
-                                            {
-                                                stack?.map( ( { id, name } ) => <Chip key={ id }>{ name }</Chip> )
-                                            }
+                                    </span>
+                                            <div className='flex gap-[12px] pt-4'>
+                                                {
+                                                    stack?.map( ( { id, name } ) => <Chip key={ id }>{ name }</Chip> )
+                                                }
 
+                                            </div>
                                         </div>
-                                    </div>
+                                    </motion.div>
                                 }
-                            </div>
+                            </AnimatePresence>
 
-                        </li>) ) }
+                        </li>
+                    )
+                )
+            }
         </ul>
+
     );
 }
 
