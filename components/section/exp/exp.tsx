d013927f24780bddc0bfa085ecc3583c07a4e9ea
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-15 11:34:12 +0600

    making custom component

--- components/section/exp/exp.tsx
+++ components/section/exp/exp.tsx
@@ -1,10 +1,10 @@
+import CustomContainer from '@/components/custom-container/custom-container';
+
 export function Exp() {
     return(
-        <div className='min-h-screen justify-center flex lg:px-[150px] py-[6%] '>
-            <div className='w-[80%] justify-start'>
+            <CustomContainer>
                 hello
-            </div>
-        </div>
+            </CustomContainer>
         );
 }
 
