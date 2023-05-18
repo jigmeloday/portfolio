55a29893b0842144bdb06926d70e77de0fba5868
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-18 15:32:17 +0600

    project listing

--- components/section/porjects/projects.tsx
+++ components/section/porjects/projects.tsx
@@ -1,19 +1,41 @@
 import CustomContainer from '@/components/custom-container/custom-container';
 import Image from 'next/image';
 import { PROJECTS } from '@/components/section/porjects/constant/porject.constant';
+import { motion } from 'framer-motion';
 
 function Projects() {
     return (
         <CustomContainer>
-            <div>Projects</div>
+            <motion.h1
+                initial={ { opacity: 0, y: - 100 } }
+                whileInView={ { opacity: 1, y: 0 } }
+                viewport={ { once: true, amount: 0.9 } }
+                transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
+                className='font-6 text-[32px] text-primary-main text-center lg:text-start'
+            >
+                Projects
+            </motion.h1>
             <div className='py-8'>
-                <span> Write up goes here</span>
-                <div className='flex flex-row flex-wrap py-8 gap-[24px] '>
+                <motion.span
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    viewport={ { once: true, amount: 0.9 } }
+                    transition={ { duration: 0.8, delay: 0.5 } }
+                    className='text-text-dark'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+                </motion.span>
+                <div className='flex lg:flex-row flex-col flex-wrap py-8 gap-[24px] '>
                     {
                         PROJECTS.map( ( { id, name, link, image, description } ) => (
-                            <div key={id} className='w-fit h-fit shadow-2xl  rounded-[8px] basis-[calc((100%/3)-24px)]'>
+                            <motion.div
+                                initial={ { opacity: 0, y: - 100 } }
+                                whileInView={ { opacity: 1, y: 0 } }
+                                viewport={ { once: true, amount: 0.9 } }
+                                transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
+                                key={id} className='w-fit h-fit shadow-2xl  rounded-[8px] basis-[calc((100%/3)-24px)]'>
                                 <div className='relative'>
-                                    <Image src={ image } alt='project' height={ 240 } className='rounded-t-[8px]'/>
+                                    <Image src={ image } alt='project' height={ 240 } className='rounded-t-[8px] hover:opacity-[0.5] transition duration-300 ease-in-out'/>
                                 </div>
                                 <div className='py-2 px-4'>
                                     <h1 className='font-6 text-[18px] text-primary-main'>{name}</h1>
@@ -24,12 +46,12 @@ function Projects() {
                                     </div>
                                     <div className='flex justify-center w-full py-4 ' >
                                         <span
-                                            className='cursor-pointer font-2 text-primary-light hover:text-primary-main transition duration-300 ease-in-out'>
+                                            className='cursor-pointer text-[12px] font-2 text-primary-light hover:text-primary-main hover:scale-[1.2] transition duration-300 ease-in-out'>
                                             Read More {link}
                                         </span>
                                     </div>
                                 </div>
-                            </div>
+                            </motion.div>
                         ) )
                     }
 
