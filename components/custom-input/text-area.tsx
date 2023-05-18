c6f9d69614f564feae29e3950d99d8aa650cfe64
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-18 21:21:54 +0600

    text area

--- components/custom-input/text-area.tsx
+++ components/custom-input/text-area.tsx
@@ -5,14 +5,14 @@ function TextArea(props: InputProps) {
     const [isFocus, setFocus] = useState<boolean>();
     const { label, name, value, onChange } = props;
     return(
-        <div className='relative  border w-fit px-2 h-[46px] rounded-[4px]'>
-            <div className='flex items-center absolute h-full'>
-                <label className={`transition delay-150 duration-200 ease-in-out ${isFocus || value ? '-translate-y-[24px] bg-[#43455c]': 'top-0'}`}>
+        <div className='relative border w-fit px-2 rounded-[4px] my-[32px]'>
+            <div className='flex absolute h-full'>
+                <label className={`transition delay-150 duration-200 ease-in-out ${isFocus || value ? '-translate-y-[14px] bg-[#43455c]': 'top-0'}`}>
                     {label}
                 </label>
             </div>
             <textarea
-                className='flex bg-transparent h-full focus:border-transparent outline-none focus:ring-0'
+                className='flex bg-transparent resize-none h-full focus:border-transparent outline-none focus:ring-0'
                 name={name}
                 onChange={onChange}
                 value={value}
