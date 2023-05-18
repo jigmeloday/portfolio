222cb84dd72fd30a667449d49864b5bd8640156f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-18 13:40:24 +0600

    project listing

---
+++ components/section/porjects/constant/porject.constant.ts
@@ -0,0 +1,47 @@
+import { Projects } from '@/components/section/porjects/model/project.model';
+import img from '../../../../public/images/naykabgokap.png';
+
+export const PROJECTS: Projects[] = [
+    {
+        id: '1',
+        name: 'Naykap Gokab',
+        link: '',
+        image: img,
+        description: 'something goes here'
+    },
+    {
+        id: '2',
+        name: 'Naykap Gokab',
+        link: '',
+        image: img,
+        description: 'something goes here'
+    },
+    {
+        id: '3',
+        name: 'Naykap Gokab',
+        link: '',
+        image: img,
+        description: 'something goes here'
+    },
+    {
+        id: '4',
+        name: 'Naykap Gokab',
+        link: '',
+        image: img,
+        description: 'something goes here'
+    },
+    {
+        id: '5',
+        name: 'Naykap Gokab',
+        link: '',
+        image: img,
+        description: 'something goes here'
+    },
+    {
+        id: '6',
+        name: 'Naykap Gokab',
+        link: '',
+        image: img,
+        description: 'something goes here'
+    }
+];
--- components/section/porjects/projects.tsx
+++ components/section/porjects/projects.tsx
@@ -1,70 +1,33 @@
 import CustomContainer from '@/components/custom-container/custom-container';
 import Image from 'next/image';
-import img from '../../../public/images/naykabgokap.png';
+import { PROJECTS } from '@/components/section/porjects/constant/porject.constant';
 
 function Projects() {
-    return(
+    return (
         <CustomContainer>
             <div>Projects</div>
             <div className='py-8'>
                 <span> Write up goes here</span>
-                <div className='flex flex-row py-8 gap-[24px]'>
-                    <div className='w-fit h-fit border rounded-[12px]'>
-                        <div className='relative'>
-                            <Image src={img} alt='project' height={240} />
-                        </div>
-                        <div className='py-2 px-4'>
-                            <h1>Project Name</h1>
-                            <span className='py-6'>
-                            hello
-                        </span>
-                            <div className='flex justify-center w-full '>
-                                Visit
+                <div className='flex flex-row flex-wrap py-8 gap-[24px] '>
+                    {
+                        PROJECTS.map( ( { id, name, link, image, description } ) => (
+                            <div key={id} className='w-fit h-fit border rounded-[12px] basis-[calc((100%/3)-24px)]'>
+                                <div className='relative'>
+                                    <Image src={ image } alt='project' height={ 240 }/>
+                                </div>
+                                <div className='py-2 px-4'>
+                                    <h1>{name}</h1>
+                                    <span className='py-6'>
+                                        {description}
+                                    </span>
+                                    <div className='flex justify-center w-full '>
+                                        {link}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className='w-fit h-fit border rounded-[12px]'>
-                        <div className='relative'>
-                            <Image src={img} alt='project' height={240} />
-                        </div>
-                        <div className='py-2 px-4'>
-                            <h1>Project Name</h1>
-                            <span className='py-6'>
-                            hello
-                        </span>
-                            <div className='flex justify-center w-full '>
-                                Visit
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-fit h-fit border rounded-[12px]'>
-                        <div className='relative'>
-                            <Image src={img} alt='project' height={240} />
-                        </div>
-                        <div className='py-2 px-4'>
-                            <h1>Project Name</h1>
-                            <span className='py-6'>
-                            hello
-                        </span>
-                            <div className='flex justify-center w-full '>
-                                Visit
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-fit h-fit border rounded-[12px]'>
-                        <div className='relative'>
-                            <Image src={img} alt='project' height={240} />
-                        </div>
-                        <div className='py-2 px-4'>
-                            <h1>Project Name</h1>
-                            <span className='py-6'>
-                            hello
-                        </span>
-                            <div className='flex justify-center w-full '>
-                                Visit
-                            </div>
-                        </div>
-                    </div>
+                        ) )
+                    }
+
                 </div>
             </div>
         </CustomContainer>
