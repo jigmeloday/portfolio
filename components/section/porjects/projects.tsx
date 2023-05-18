d9a4c22a6a724ffa89ce46108a0b91f3ec5ece26
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-18 12:51:53 +0600

    project layouting

---
+++ components/section/porjects/projects.tsx
@@ -0,0 +1,11 @@
+import CustomContainer from '@/components/custom-container/custom-container';
+
+function Projects() {
+    return(
+        <CustomContainer>
+            <div>Projects</div>
+        </CustomContainer>
+    );
+}
+
+export default Projects;
