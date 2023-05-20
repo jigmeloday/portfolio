2af79fdb75421a1dc1c5b7e05f186349be9aa0b9
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-20 14:38:50 +0600

    validation

--- components/section/contact/schema/contact.schema.ts
+++ components/section/contact/schema/contact.schema.ts
@@ -1,6 +1,12 @@
 import * as Yup from 'yup';
 
 export const CONTACT_SCHEMA = Yup.object().shape( {
+    name: Yup.string()
+        .required('Name Must'),
     email: Yup.string()
         .required( 'REQUIRED_FIELD' ).email( 'EMAIL_FIELD' ),
+    phone: Yup.string()
+        .required('Phone Number Must'),
+    message: Yup.string()
+        .required('required')
 } );
