42a66bc8591095e0b5c85109143e187ef8a25891
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-29 16:51:09 +0600

    skill

--- components/section/skills/skills.tsx
+++ components/section/skills/skills.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 function Skills() {
     return(
-        <div className='min-h-screen justify-center flex lg:px-[150px] py-[6%] '>
+        <div className='justify-center flex lg:px-[150px] py-[6%] '>
             <div className='w-[80%] justify-start'>
                 <motion.h1
                     initial={ { opacity: 0, y: - 100 } }
@@ -13,6 +13,12 @@ function Skills() {
                 >
                     Skills
                 </motion.h1>
+                <span>
+                    I possess a diverse range of skills that I regularly utilize, and I have highlighted some of them below. If you would like to learn more about my capabilities, I encourage you to refer to my CV, which provides a comprehensive overview of my experience and expertise, including skills that may not be mentioned here. I am proud to say that I have successfully applied these skills in over five projects as of 2022.
+                </span>
+                <div>
+
+                </div>
             </div>
         </div>
     );
