30c4e0ac033e31d647108ef1c4b3a7efa8eb1664
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-05-31 12:22:28 +0600

    footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,10 @@
 function Footer() {
+    const date = new Date();
     return(
         <div className='py-[22px] font-3 text-[14px] text-center' >
             <div className='flex justify-center'>
                 <div className='w-[86%] text-[12px] font-2'>
-                    ©Copyright ©2023 All rights reserved
+                    ©Copyright ©{date.getFullYear()} All rights reserved
                 </div>
             </div>
         </div>
