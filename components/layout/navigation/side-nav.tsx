2a40801bbe24af1770bbc24e8eb00e0c08d42742
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-01 12:53:24 +0600

    revamp tailwind config set up

--- components/layout/navigation/side-nav.tsx
+++ components/layout/navigation/side-nav.tsx
@@ -1,30 +1,73 @@
-import { NAV_CONST } from '@/components/icons/icons';
-import { AnimatePresence, motion } from 'framer-motion';
+import { NAV_CONST, SOCIAL_ICONS } from '@/components/icons/icons';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import img from '@/public/images/profile.png';
 
 function SideNav() {
     return(
-        <AnimatePresence>
+        <div className='lg:block shadow-2xl w-[240px]  fixed hidden top-[10%] left-[6%] overflow-hidden rounded-[12px] px-2 py-6 text-primary-main'>
+            <div className='flex flex-col items-center'>
+                <motion.div
+                    initial={{ y:-100  }}
+                    animate={{ y:0 }}
+                    transition={{ duration: 0.9 }}
+                    className='flex items-center justify-center bg-primary-dark shadow-2xl relative rounded-full w-[150px] h-[150px] overflow-hidden' >
+                    <Image src={img} alt='profile' height={240}  className='rounded-full'  />
+                </motion.div>
+                <div className='pt-[24px]'>
+                    <motion.h1
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1, delay: 0.5 }}
+                        className='text-[24px] text-center font-8'> Jigme Lodey </motion.h1>
+                    <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1.2, delay: 0.7 }}
+                        className='pt-[8px] text-center text-[14px]'>
+                        I am Software
+                        <span className='text-primary-main pl-[6px]'>Developer</span>
+                    </motion.span>
+                </div>
+            </div>
             <motion.div
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.5 }}
-                className='lg:block drop-shadow-2xl fixed hidden top-[30%] left-[8%] overflow-hidden bg-primary-dark rounded-[12px] px-2 py-6 text-primary-main'>
+                className='overflow-hidden  px-2 py-4 text-primary-main'>
                 {
-                    NAV_CONST.map(({ icon, id}) => (
+                    NAV_CONST.map(({ label, id}) => (
                         <motion.div
                             key={id}
                             initial={{ y: -200, opacity: 0 }}
                             animate={{ y: 0, opacity: 1 }}
                             transition={{ delay: 0.5, duration: 1 }}
-                            className='flex justify-center px-[4px] py-[8px] '>
-                            <div className='text-[18px] items-center cursor-pointer hover:text-primary-light hover:scale-150 transition delay-1500 duration-500 ease-in-out'>
-                                {icon}
+                            className='flex justify-center px-[4px] py-[6px] '>
+                            <div className='text-[14px] font-6 items-center cursor-pointer hover:text-primary-light hover:scale-150 transition delay-1500 duration-500 ease-in-out'>
+                                {label}
                             </div>
                         </motion.div>
                     ))
                 }
             </motion.div>
-        </AnimatePresence>
+            <div className='flex w-full justify-center py-[14px]'>
+                {
+                    SOCIAL_ICONS.map(({ id, name }) =>(
+                        <motion.div
+                            key={id}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 1.4, delay: 0.9 }}
+                            className='text-[20px] text-center cursor-pointer shadow-2xl text-primary-main hover:scale-150 translation duration-300 ' >
+                            {name}
+                        </motion.div>
+                    ))
+                }
+            </div>
+            <div className='flex items-end justify-center'>
+                Footer
+            </div>
+        </div>
     );
 }
 
