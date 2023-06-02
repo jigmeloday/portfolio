47e9a6e28d0ac8dd3afb9c83aaf54d2ff541e9e1
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-02 13:59:15 +0600

    exp revamping

--- components/section/exp/exp.tsx
+++ components/section/exp/exp.tsx
@@ -1,30 +1,35 @@
 import CustomContainer from '@/components/custom-container/custom-container';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import StepperComponent from '@/components/section/exp/stepper-component/stepper-component';
+import CustomHeader from '@/components/custom-header/custom-header';
 
 export function Exp() {
     return (
-        <AnimatePresence>
             <CustomContainer>
-                <motion.h1
-                    initial={ { opacity: 0, y: - 100 } }
-                    whileInView={ { opacity: 1, y: 0 } }
-                    viewport={ { once: true, amount: 0.9 } }
-                    transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
-                    className='font-6 text-[32px] text-primary-main text-center lg:text-start'
-                >
-                   My Journey.
-                </motion.h1>
-                <motion.div
-                    initial={ { opacity: 0 } }
-                    whileInView={ { opacity: 1 } }
-                    viewport={ { once: true, amount: 0.9 } }
-                    transition={ { delay: 0.5 } }
-                    className='py-8'>
-                    <StepperComponent />
-                </motion.div>
+               <div className='px-[32px]'>
+                   <motion.div
+                       initial={ { opacity: 0, y: - 100 } }
+                       whileInView={ { opacity: 1, y: 0 } }
+                       viewport={ { once: true, amount: 0.9 } }
+                       transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
+                   >
+                       <CustomHeader text='MY EXPERIENCE' />
+                   </motion.div>
+                   <div className='pt-[32px]'>
+                       <h1 className='font-6 text-[24px] py-[12px]'>
+                           My Journey
+                       </h1>
+                       <motion.div
+                           initial={ { opacity: 0 } }
+                           whileInView={ { opacity: 1 } }
+                           viewport={ { once: true, amount: 0.9 } }
+                           transition={ { delay: 0.5 } }
+                           className='py-8'>
+                           <StepperComponent />
+                       </motion.div>
+                   </div>
+               </div>
             </CustomContainer>
-        </AnimatePresence>
     );
 }
 
