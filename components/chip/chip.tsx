2ed51b3eefe368cb5343457cdf379e647c31afc0
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-02 21:55:38 +0600

    ui about me mobile view

--- components/chip/chip.tsx
+++ components/chip/chip.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function Chip(props: { children: ReactNode }) {
     return(
-        <div className='font-2 border-[1px] w-fit px-[8px] border-primary-light shadow-lg rounded-[8px]'>
+        <div className='font-2 border-[1px] flex justify-center items-center text-[12px] lg:text-[16px] w-fit px-[8px] border-primary-light shadow-lg rounded-[8px]'>
             {props.children}
         </div>
     );
