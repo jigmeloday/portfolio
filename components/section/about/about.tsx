30e21e9c78963df720cafac9aa8d2dd165b9b581
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-02 12:59:32 +0600

    about revamping

--- components/section/about/about.tsx
+++ components/section/about/about.tsx
@@ -1,55 +1,49 @@
-// import { motion } from 'framer-motion';
-// import { ABOUT_BOX, DUMMY } from '@/components/section/about/about.constant';
+import { motion } from 'framer-motion';
 import CustomContainer from '@/components/custom-container/custom-container';
 import CustomHeader from '@/components/custom-header/custom-header';
+import { DUMMY } from '@/components/section/about/about.constant';
 
 function About() {
     return (
             <CustomContainer>
-
-                {/*<motion.div*/}
-                {/*    initial={ { opacity: 0, y: - 100 } }*/}
-                {/*    whileInView={ { opacity: 1, y: 0 } }*/}
-                {/*    viewport={ { once: true, amount: 0.8 } }*/}
-                {/*    transition={ { type: 'spring', bounce: 0.1, delay: 0.6 } }*/}
-                {/*    className='py-[32px] text-justify text-text-main'>*/}
-                {/*    { DUMMY }*/}
-                {/*</motion.div>*/}
-                {/*<div className='flex flex-col  items-center md:flex-row py-8  gap-[12px] justify-center lg:justify-between'>*/}
-                {/*    {*/}
-                {/*        ABOUT_BOX.map( ( { id, icon, title, description } ) => (*/}
-                {/*            <motion.div*/}
-                {/*                initial={{ opacity: 0, y: -100 }}*/}
-                {/*                whileInView={{ opacity: 1, y: 0 }}*/}
-                {/*                viewport={{ once: true, amount: 0.8 }}*/}
-                {/*                transition={{ type: 'spring', bounce: 0.1, delay: 0.7 }}*/}
-                {/*                key={id}*/}
-                {/*                className='w-[250px] h-[200px] overflow-hidden transition duration-150 group relative rounded-[16px]'*/}
-                {/*            >*/}
-                {/*                <div className='flex flex-col min-h-full bg-primary-main shadow-2xl relative overflow-hidden justify-center items-center'>*/}
-                {/*                    <div className='text-primary-main py-[8px]'>*/}
-                {/*                            <span className='text-[38px]'>*/}
-                {/*                                {icon}*/}
-                {/*                            </span>*/}
-                {/*                    </div>*/}
-                {/*                    <span className='font-3 text-[14px] py-[12px]'>*/}
-                {/*                            {title}*/}
-                {/*                        </span>*/}
-                {/*                </div>*/}
-                {/*                <div className='min-h-full w-full items-center justify-center bg-hover-main absolute flex top-60 group-hover:top-0 opacity-0 group-hover:opacity-100 transition-all duration-500'>*/}
-                {/*                    <div className='p-[12px] '>*/}
-                {/*                           <span className='text-text-light font-2 text-[12px] '>*/}
-                {/*                                {description}*/}
-                {/*                           </span>*/}
-                {/*                    </div>*/}
-                {/*                </div>*/}
-                {/*            </motion.div>*/}
-                {/*        ) )*/}
-                {/*    }*/}
-                {/*</div>*/}
-
                 <div className='px-[32px]'>
-                    <CustomHeader text='About Me' />
+                    <motion.div
+                        initial={ { opacity: 0, x: -100 } }
+                        whileInView={ { opacity: 1, x: 0 } }
+                        viewport={ { once: true, amount: 0.9 } }
+                        transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
+                    >
+                        <CustomHeader text='About Me' />
+                    </motion.div>
+                    <div className='py-[34px]'>
+                        {
+                            DUMMY.map(({ title, description }) => (
+                                <div
+                                    key={title}
+                                    className='py-[8px]'
+                                >
+                                    <motion.h1
+                                        initial={ { opacity: 0, x: -100 } }
+                                        whileInView={ { opacity: 1, x: 0 } }
+                                        viewport={ { once: true, amount: 0.9 } }
+                                        transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
+                                        className='font-6 text-[24px] py-[12px]'>
+                                        {title}
+                                    </motion.h1>
+                                    <motion.div
+                                        initial={ { opacity: 0, x: -100 } }
+                                        whileInView={ { opacity: 1, x: 0 } }
+                                        viewport={ { once: true, amount: 0.9 } }
+                                        transition={ { type: 'spring', bounce: 0.1, delay: 0.5 } }
+                                        className='px-[8px]'>
+                                        <span className='font-4 text-[14px]'>
+                                        {description}
+                                    </span>
+                                    </motion.div>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </CustomContainer>
 
