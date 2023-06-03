c59356639b89a0fd557163d2d73bfbdf581feb50
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-03 17:02:22 +0600

    ui mobile view skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Landing from '@/components/section/home/home';
 import About from '@/components/section/about/about';
 import Exp from '@/components/section/exp/exp';
+import Skills from '@/components/section/skills/skills';
 // import Projects from '@/components/section/porjects/projects';
 // import Contact from '@/components/section/contact/contact';
-// import Skills from '@/components/section/skills/skills';
 
 export default function Home() {
     return (
@@ -17,9 +17,9 @@ export default function Home() {
             <section>
                 <Exp />
             </section>
-            {/*<section>*/}
-            {/*    <Skills />*/}
-            {/*</section>*/}
+            <section>
+                <Skills />
+            </section>
             {/*<section>*/}
             {/*    <Projects />*/}
             {/*</section>*/}
