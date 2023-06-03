b250aaf20d4d2acb0030f8dae64891e3049d2068
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-03 14:55:14 +0600

    ui  mobile view

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentNav, setNavState } from '@/services/state/shared/shared.slice';
 import Drawer from '@/components/layout/navigation/drawer';
 import SideNav from '@/components/layout/navigation/side-nav';
+import { FaBars } from 'react-icons/fa';
 
 function Layout(props: { children: ReactNode }) {
     const isOpen = useSelector(selectCurrentNav);
@@ -13,8 +14,11 @@ function Layout(props: { children: ReactNode }) {
               <div className='flex items-center min-h-screen fixed left-[6%] '>
                   <SideNav />
               </div>
-              <div>
-                  <button className='lg:hidden text-white absolute' onClick={() => dispatch(setNavState(!isOpen))}>press </button>
+              <div className='relative'>
+                  <div className=' text-[24px] text-primary-main flex justify-center items-center top-[30px] w-[70px] border fixed py-[20px] shadow-2xl rounded-r-[10px]' onClick={() => dispatch(setNavState(!isOpen))}>
+                      <FaBars />
+                  </div>
+                  {/*<button className='lg:hidden text-white absolute' onClick={() => dispatch(setNavState(!isOpen))}>press </button>*/}
                   {props.children}
               </div>
           </div>
