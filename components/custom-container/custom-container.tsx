ef3bc3c2669c84eaf6446c6239e433399dac6112
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-06 00:24:22 +0600

    aniimation fixes

--- components/custom-container/custom-container.tsx
+++ components/custom-container/custom-container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function CustomContainer(props: { children: ReactNode }) {
     return(
-       <div className='flex lg:min-h-screen justify-start lg:justify-end py-[42px]'>
+       <div className='flex  justify-start lg:justify-end py-[42px]'>
            <div className='lg:w-[74%] w-full px-[16px] md:px-[40px] lg:px-[8px] '>
                {props.children}
            </div>
