5c57b5ebaa9ebe60d2da2c9093d410cfd591cb6c
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-05 17:56:16 +0600

    ui animation

--- components/custom-header/custom-header.tsx
+++ components/custom-header/custom-header.tsx
@@ -3,10 +3,10 @@ import { motion } from 'framer-motion';
 function CustomHeader(props: { text: string }) {
     return(
             <motion.h5
-                initial={ { opacity: 0, x: -150} }
-                whileInView={ { opacity: 1, x: 0 } }
+                initial={ { opacity: 0, y: 120} }
+                whileInView={ { opacity: 1, y:0 } }
                 viewport={ { once: true, amount: 0.9 } }
-                transition={ { delay: 0.5, duration: 1.2 } }
+                transition={ { delay: 0.2, type: 'spring', bounce: 0.6 } }
                 className='font-4 text-[14px] md:text-[18px] text-primary-header text-start'
             >
                 { props.text.toUpperCase() }
