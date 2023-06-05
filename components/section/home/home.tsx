8425df838940234fce12ec2911d1183c1bbd8f82
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-05 12:49:35 +0600

    ui fixes

--- components/section/home/home.tsx
+++ components/section/home/home.tsx
@@ -91,41 +91,80 @@ function Landing(){
         //         </div>
         //     </div>
         // </div>
-        <CustomContainer>
-            <div className='flex'>
-                <div className='flex flex-col justify-center min-h-screen w-[100%] '>
-                   <h3 className='font-6 text-[16px] md:text-[24px]'>
-                       HI, THERE! 👋 I AM
-                   </h3>
-                    <div className='py-[12px] lg:py-[4px]'>
-                        <h2 className='font-7 text-[20px] md:text-[32px] text-primary-main'>
-                            Jigme Lodey
-                        </h2>
-                    </div>
-                    <div className='md:w-[400px] py-[14px]'>
+        // <CustomContainer>
+        //     <div className='flex'>
+        //         <div className='flex flex-col justify-center min-h-screen w-[100%] '>
+        //            <h3 className='font-6 text-[16px] md:text-[24px]'>
+        //                HI, THERE! 👋 I AM
+        //            </h3>
+        //             <div className='py-[12px] lg:py-[4px]'>
+        //                 <h2 className='font-7 text-[20px] md:text-[32px] text-primary-main'>
+        //                     Jigme Lodey
+        //                 </h2>
+        //             </div>
+        //             <div className='md:w-[400px] py-[14px]'>
+        //                 <span className='text-[12px] md:text-[16px] leading-normal '>
+        //                     I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences.
+        //                 </span>
+        //                 <div className='flex mt-[12px] py-[4px] items-center'>
+        //                     <span className='text-[16px] md:text-[24px]'>
+        //                         <MdKeyboardArrowRight />
+        //                     </span>
+        //                     <span className='text-primary-main text-[10px] md:text-[18px]'> {typeWriter} <span className='animate-pulse text-[10px] md:text-[16px]'>_</span> </span>
+        //                 </div>
+        //                 <div className='my-[20px]'>
+        //                     <Button className='h-[40px] w-[80px] font-4 text-[14px] md:h-[50px] md:w-[120px] md:text-[18px]' label='Hire Me' type='button' variant='contained' click={() => alert('hello')}  />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div className='flex flex-col justify-center min-h-screen w-full'>
+        //           <div className='flex  justify-center'>
+        //               <div className='flex items-center justify-center  relative w-full h-[150px] md:w-[300px] md:h-[300px]'>
+        //                   <Image src={img} alt='profile' height={540}  className='rounded-full'  />
+        //               </div>
+        //           </div>
+        //         </div>
+        //     </div>
+        // </CustomContainer>
+        <div className='flex min-h-screen justify-start lg:justify-end py-[42px]'>
+            <div className='lg:w-[74%] w-full md:px-[16px] md:px-[40px] lg:px-[8px] '>
+                <div className='flex flex-col items-center lg:items-start md:py-[44px] py-[26px] px-[32px] lg:px-[0]'>
+                    <div className='flex'>
+                        <div className='flex flex-col justify-center min-h-screen w-[100%] '>
+                            <h3 className='font-6 text-[16px] md:text-[24px]'>
+                                HI, THERE! 👋 I AM
+                            </h3>
+                            <div className='py-[12px] lg:py-[4px]'>
+                                <h2 className='font-7 text-[20px] md:text-[32px] text-primary-main'>
+                                    Jigme Lodey
+                                </h2>
+                            </div>
+                            <div className='md:w-[400px] py-[14px]'>
                         <span className='text-[12px] md:text-[16px] leading-normal '>
                             I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences.
                         </span>
-                        <div className='flex mt-[12px] py-[4px] items-center'>
+                                <div className='flex mt-[12px] py-[4px] items-center'>
                             <span className='text-[16px] md:text-[24px]'>
                                 <MdKeyboardArrowRight />
                             </span>
-                            <span className='text-primary-main text-[10px] md:text-[18px]'> {typeWriter} <span className='animate-pulse text-[10px] md:text-[16px]'>_</span> </span>
+                                    <span className='text-primary-main text-[10px] md:text-[18px]'> {typeWriter} <span className='animate-pulse text-[10px] md:text-[16px]'>_</span> </span>
+                                </div>
+                                <div className='my-[20px]'>
+                                    <Button className='h-[40px] w-[80px] font-4 text-[14px] md:h-[50px] md:w-[120px] md:text-[18px]' label='Hire Me' type='button' variant='contained' click={() => alert('hello')}  />
+                                </div>
+                            </div>
                         </div>
-                        <div className='my-[20px]'>
-                            <Button className='h-[40px] w-[80px] font-4 text-[14px] md:h-[50px] md:w-[120px] md:text-[18px]' label='Hire Me' type='button' variant='contained' click={() => alert('hello')}  />
+                        <div className='flex flex-col justify-center min-h-screen w-full'>
+                            <div className='flex  justify-center'>
+                                <div className='flex items-center justify-center  relative w-full h-[150px] md:w-[300px] md:h-[300px]'>
+                                    <Image src={img} alt='profile' height={540}  className='rounded-full'  />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-col justify-center min-h-screen w-full'>
-                  <div className='flex  justify-center'>
-                      <div className='flex items-center justify-center  relative w-full h-[150px] md:w-[300px] md:h-[300px]'>
-                          <Image src={img} alt='profile' height={540}  className='rounded-full'  />
-                      </div>
-                  </div>
-                </div>
             </div>
-        </CustomContainer>
+        </div>
     );
 }
 
