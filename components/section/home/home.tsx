efe7e3ca4ac4ecf99fc8a88d9190fb82dc4e1db6
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-06-05 19:28:45 +0600

    ui animation

--- components/section/home/home.tsx
+++ components/section/home/home.tsx
@@ -9,85 +9,6 @@ function Landing(){
     // const getTime = Weather();
     const typeWriter = useTypedSuperpower(['User Experience Designer', 'User Interface Developer', 'Frontend Developer', 'Backend Developer']) ;
     return(
-        // <div className='flex min-h-screen w-screen justify-center md:justify-end lg:px-[42px] px-[12px]'>
-        //     <div className='flex w-screen lg:w-[76%] '>
-        //         <div className='flex flex-col justify-center lg:px-[22px] lg:w-[40%] '>
-        //             <motion.span
-        //                 initial={{ opacity: 0 }}
-        //                 whileInView={{ opacity: 1 }}
-        //                 transition={{ delay: 0.3, duration: 0.9 }}
-        //                 viewport={ { once: true, amount: 0.9 } }
-        //                 className='pt-[6px] pb-[4px] font-5 text-[18px]'>
-        //                 HI, THERE! 👋 I AM
-        //             </motion.span>
-        //             <motion.span
-        //                 initial={{ opacity: 0 }}
-        //                 whileInView={{ opacity: 1 }}
-        //                 transition={{ delay: 0.5, duration: 0.9 }}
-        //                 viewport={ { once: true, amount: 0.9 } }
-        //                 className='pb-[4px] font-6 text-[28px] lg:text-[32px] text-primary-main'>
-        //                 Jigme Lodey
-        //             </motion.span>
-        //             <motion.span
-        //                 initial={{ opacity: 0 }}
-        //                 whileInView={{ opacity: 1 }}
-        //                 transition={{ delay: 0.7, duration: 0.9 }}
-        //                 viewport={ { once: true, amount: 0.9 } }
-        //                 className='py-[8px] lg:font-3 font-4 text-[14px]'>
-        //                 I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences.
-        //             </motion.span>
-        //             <motion.div
-        //                 initial={{ opacity: 0 }}
-        //                 whileInView={{ opacity: 1 }}
-        //                 transition={{ delay: 0.9, duration: 0.9 }}
-        //                 viewport={ { once: true, amount: 0.9 } }
-        //                 className='flex pt-[16px] items-center h-[34px]'>
-        //                <span className='text-center text-[18px] lg:text-[24px]'>
-        //                    <MdKeyboardArrowRight />
-        //                </span>
-        //                 <span className='text-primary-main text-[14px] lg:text-[16px]'> {typeWriter} </span>
-        //                 <span className='animate-pulse text-[14px] lg:text-[16px]'>_</span>
-        //             </motion.div>
-        //             <motion.div
-        //                 initial={{ y: 100, opacity: 0 }}
-        //                 whileInView={{ y: 0, opacity: 1 }}
-        //                 transition={{ delay: 0.9, type:'spring', bounce: 0.8 }}
-        //                 viewport={ { once: true, amount: 0.3, } }
-        //                 className='py-[20px]'
-        //             >
-        //                 <Button className='w-[70px] lg:w-[100px] text-[12px]' label='Hire Me' type='button' variant='contained' click={() => alert('hello')} />
-        //             </motion.div>
-        //         </div>
-        //         <div className='flex lg:items-center justify-center w-full pr-[24px] relative'>
-        //             <motion.div
-        //                 initial={{ x: -200, opacity:0 }}
-        //                 whileInView={{ x: 0, opacity: 1 }}
-        //                 transition={{ delay: 0.9, duration: 5.9 }}
-        //                 viewport={ { once: true, amount: 0.3, } }
-        //                 className='flex block lg:hidden items-center justify-center  relative'>
-        //                 <Image src={img} alt='profile' height={540}  className='rounded-full'  />
-        //             </motion.div>
-        //             <motion.div
-        //                 initial={{ x: -500, opacity:0 }}
-        //                 whileInView={{ x: 0, opacity: 1 }}
-        //                 transition={{ delay: 0.9, duration: 5.9 }}
-        //                 viewport={ { once: true, amount: 0.3, } }
-        //                 className='flex items-center hidden lg:block justify-center  relative w-[300px] h-[300px] overflow-hidden'>
-        //                 <Image src={img} alt='profile' height={540}  className='rounded-full'  />
-        //             </motion.div>
-        //             <motion.div
-        //                 initial={{ scale:0, opacity:0.5 }}
-        //                 whileInView={{ scale: 1, opacity: 1 }}
-        //                 transition={{ delay: 0.9, duration: 5.9 }}
-        //                 viewport={ { once: true, amount: 0.3, } }
-        //                 className='absolute right-[60%] top-[34%] lg:top-[28%]'>
-        //                 {
-        //                     getTime === 'night' ? <BsFillMoonFill className='text-text-moon text-[20px] lg:text-[32px]' /> : <BsFillSunFill className='text-text-sun text-[24px] lg:text-[42px]' />
-        //                 }
-        //             </motion.div>
-        //         </div>
-        //     </div>
-        // </div>
         <div className='flex min-h-screen justify-start lg:justify-end py-[42px]'>
             <div className='lg:w-[74%] w-full md:px-[16px] md:px-[40px] lg:px-[8px] '>
                 <div className='flex flex-col items-center lg:items-start md:py-[44px] py-[26px] px-[32px] lg:px-[0]'>
@@ -115,7 +36,7 @@ function Landing(){
                                 <motion.div
                                     initial={{ opacity: 0, y: 80}}
                                     whileInView={{ opacity: 1, y: 0}}
-                                    transition={{ delay: 2.7, duration: 1.2 }}
+                                    transition={{ delay: 0.6, duration: 1.5 }}
                                     viewport={ { once: true, amount: 0.9 } }
                                 >
                                     <span
@@ -126,7 +47,7 @@ function Landing(){
                                 <motion.div
                                     initial={{ opacity: 0, x: -20}}
                                     whileInView={{ opacity: 1, x:0}}
-                                    transition={{ delay: 3.5, duration: 1.5 }}
+                                    transition={{ delay: 1.2, duration: 1.5 }}
                                     viewport={ { once: true, amount: 0.9 } }
                                     className='flex mt-[12px] py-[4px] items-center'>
                                     <span className='text-[16px] md:text-[24px]'>
@@ -137,7 +58,7 @@ function Landing(){
                                 <motion.div
                                     initial={{ opacity: 0, y: 30}}
                                     whileInView={{ opacity: 1, y:0}}
-                                    transition={{ delay: 4.1, type: 'spring', bounce: 0.8}}
+                                    transition={{ delay: 2.1, type: 'spring', bounce: 0.8}}
                                     viewport={ { once: true, amount: 0.9 } }
                                     className='my-[20px]'>
                                     <Button className='h-[40px] w-[80px] font-4 text-[14px] md:h-[50px] md:w-[120px] md:text-[18px]' label='Hire Me' type='button' variant='contained' click={() => alert('hello')}  />
@@ -154,7 +75,7 @@ function Landing(){
                                 <motion.div
                                     initial={{ x: -200 }}
                                     whileInView={{ x: 0 }}
-                                    transition={{ delay: 0.2, duration: 7.2}}
+                                    transition={{ delay: 0.2, duration: 2.5}}
                                     viewport={ { once: true, amount: 0.2 } }
                                     className='flex z-0 items-center justify-center  relative w-full h-[150px] md:w-[300px] md:h-[300px]'>
                                     <Image src={img} alt='profile' height={540}  className='rounded-full'  />
